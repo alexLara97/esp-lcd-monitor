435013f8bd5b30af516e7694428df9f2758ba408
Alejandro Lara <alex.lara.cont97@gmail.com>
2025-09-10 19:23:34 +0200

    Setup y loop implementation

--- src/main.cpp
+++ src/main.cpp
@@ -54,3 +54,80 @@ void showStatusLine2(const String& msg) {
   lcd.print(line);
 }
 
+void setup() {
+  Serial.begin(115200);
+  delay(200);
+
+  setupLCD();
+  connectToWifi();
+
+  if (udp.begin(UDP_PORT)) {
+    lcd.setCursor(0,0);
+    lcd.print("UDP on port: ");
+    lcd.print(UDP_PORT);
+  } else {
+    Serial.println("Error to start UDP");
+    lcd.setCursor(0, 0);
+    lcd.print("Error init UDP");
+  }
+}
+
+void loop() {
+  int packetSize = udp.parsePacket();
+  if (packetSize > 0) {
+    if (packetSize >= (int)UDP_BUFFER_SIZE) {
+      Serial.println("Package too large. Download....");
+      // Vacía el paquete para no bloquear el buffer
+      while (udp.available()) udp.read();
+      showStatusLine2("Pkt too large");
+      return;
+    }
+    int len = udp.read(udpBuffer, UDP_BUFFER_SIZE - 1);
+    if (len < 0) return;
+    udpBuffer[len] = '\0';
+
+    Serial.print("UDP recevied");
+    Serial.print(udpBuffer);
+
+    // JSON --> {"cpu": 45.2, "mem": 62.7, "temp": 55.3}
+    StaticJsonDocument<256> doc;
+    DeserializationError err = deserializeJson(doc, udpBuffer);
+    if (err) {
+      Serial.print("Error JSON: ");
+      Serial.println(err.c_str());
+      showStatusLine2("JSON error");
+      return;
+    }
+
+    // data extraction with default values
+    float cpu = doc["cpu"] | -1.0;
+    float mem = doc["mem"] | -1.0;
+    float temp = doc["temp"] | -1.0;
+
+    // LCD Show
+    lcd.setCursor(0,0);
+    // Line 1: CPU: x MEM: x
+    char line1[17];
+    snprintf(line1, sizeof(line1), "CPU:%4.1f MEM:%2.0f", cpu, mem);
+    lcd.print(line1);
+
+    int fill1 = 16 - strlen(line1);
+    while (fill1-- > 0) lcd.print(' ');
+
+    // Line 2: TMP: XC IP final
+    lcd.setCursor(0, 1);
+    char line2[17];
+    snprintf(line2, sizeof(line2), "TMP:%4.1fC", temp);
+    lcd.print(line2);
+
+    int fill2 = 16 - strlen(line2);
+    while (fill2-- > 0) lcd.print(' ');
+    
+    // Optional: respond or doing ack (UDP is connectionless, but you can respond)
+    // udp.beginPacket(udp.remoteIP(), udp.remotePort());
+    // udp.print("OK");
+    // udp.endPacket();
+  }
+
+  delay(5);
+}
